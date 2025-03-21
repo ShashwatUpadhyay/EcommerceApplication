53cf3d7567e2ab40cea43759b81d2ad84a7f7077
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-03-22 04:16:16 +0530

    changes

--- base/base_model.py
+++ base/base_model.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 
 class BaseModel(models.Model):
-    uid = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
+    uid = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,48 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse , redirect
 from . import models
+from django.contrib import messages
 # Create your views here.
 
 def shop(request):
     category = models.ProductCategory.objects.all()
     products = models.Product.objects.all()
+    
+    category_name = request.GET.get('category')
+    sort = request.GET.get('sort')
+    
+    if category_name == 'all':
+        return render(request , 'shop.html',{'products':products, 'categories': category})
+
+    category_obj = None
+    try:
+        category_obj = models.ProductSubCategory.objects.get(name=category_name)
+        products = models.Product.objects.filter(subcategory = category_obj)
+        if sort == 'az':
+            products = models.Product.objects.filter(subcategory = category_obj).order_by('title')
+            print(sort)
+        elif sort == 'new':
+            products = models.Product.objects.filter(subcategory = category_obj).order_by('created_at')
+            print(sort)
+            
+        elif sort == 'lh':
+            products = models.Product.objects.filter(subcategory = category_obj).order_by('price')
+            print(sort)
+            
+        elif sort == 'hl':
+            products = models.Product.objects.filter(subcategory = category_obj).order_by('-price')
+            print(sort)
+            
+        return render(request , 'shop.html',{'products':products, 'categories': category})
+            
+    except:
+        pass
+    try:
+        category_obj = models.ProductCategory.objects.get(name=category_name)
+        products = models.Product.objects.filter(category = category_obj)
+        return render(request , 'shop.html',{'products':products, 'categories': category})
+    except:
+        messages.error(request , "Product Not Found")
+    
     return render(request , 'shop.html',{'products':products, 'categories': category})
 
 def product_page(request, slug):
