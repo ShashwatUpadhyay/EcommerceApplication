c772d7d8461a0c7df6c63194ac24b1142892e43c
tejashxv <tejashyadavcr7@gmail.com>
2025-03-22 19:30:35 +0530

    home domne

--- base/views.py
+++ base/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from shop.models import *
+from itertools import groupby
+from operator import attrgetter
+from math import ceil
 # Create your views here.
  
 def home(request):
-    products = Product.objects.all()
+    categories = ProductCategory.objects.all()
+    products = [category.products.first() for category in categories if category.products.exists()]
+    
+    slides = [products[i:i+4] for i in range(0, len(products), 4)]
     cat = ProductCategory.objects.all().distinct()
     print(cat)
     context = {
         'products':products,
-        'catogery':cat
+        'catogery':cat,
+        'slides': slides
     }
     return render(request,'home.html',context)
