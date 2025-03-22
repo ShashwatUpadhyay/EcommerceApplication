4795814bbae8749ce6f5e8a7b71473be96b3adff
tejashxv <tejashyadavcr7@gmail.com>
2025-03-22 17:15:33 +0530

    made home page responsive

--- shop/models.py
+++ shop/models.py
@@ -6,6 +6,7 @@ from django.utils.text import slugify
 class ProductCategory(BaseModel):
     name = models.CharField(max_length=50)
     slug = models.CharField(max_length=50, null=True, blank=True)
+    img = models.ImageField(upload_to='category_images/', null=True, blank=True)
     
     def __str__(self):
         return str(self.name) 
