92fdcecda0e06a0048d1035b4d9bd1c52c2a9006
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-07-17 18:52:48 +0530

    changes

--- ecom/settings.py
+++ ecom/settings.py
@@ -92,9 +92,16 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+          'ENGINE':'django.db.backends.mysql',
+          'NAME':os.getenv('DB_NAME'),
+          'USER':os.getenv('DB_USER'),
+          'PASSWORD':os.getenv('DB_PASSWORD'),
+          'HOST':'localhost',
+          'PORT':'3306',
+          'OPTIONS': {
+            'charset': 'utf8mb4',  # This is the important part
+        },
+      },
 }
 
 
