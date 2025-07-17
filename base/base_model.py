f2ac64da1ce9d53a3f9b322fbc574064c2006ddd
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-07-17 19:12:11 +0530

    re migrated all data base

--- base/base_model.py
+++ base/base_model.py
@@ -2,7 +2,8 @@ from django.db import models
 import uuid
 
 class BaseModel(models.Model):
-    uid = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    id = models.AutoField(primary_key=True)
+    uid = models.UUIDField(default=uuid.uuid4,editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
